660b004f7e18b714fd76ee298fb73855fe535b6e
Dreamy-Z3r0 <danielblake98vt@gmail.com>
2020-09-05 18:04:25 +0700

    add general info for ESP8266 firmware

--- ESP8266-firmware/ESP8266-firmware.ino
+++ ESP8266-firmware/ESP8266-firmware.ino
@@ -1,4 +1,17 @@
+/*
+ * Generic ESP8266 works as offline server with following functions:
+ *  + Receiving user intakes for date time setup.
+ *  + Displaying and receiving user's desired volume for speaker.
+ *
+ * Default IP address: 192.168.4.1
+ * Server access info:
+ *  + SSID: #DigitalClock
+ *  + Password: 123456789
+ *
+ */
 
+const *char[] = "#DigitalClock;
+const *char[] = "123456789";
 
 void setup ()
 {
